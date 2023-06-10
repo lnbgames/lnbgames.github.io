2b561cd0103b15b4b306978a1d4b87a00f63bf1a
lnbgames <127859098+lnbgames@users.noreply.github.com>
2023-06-10 18:55:49 -0500

    Update test.php

--- test.php
+++ test.php
@@ -1,2 +1,11 @@
-$dom = new DOMDocument();
-@$dom->loadHTML($test.html);
+<?php
+// Load the HTML template
+$dom = new DOMDocument;
+$dom->loadHTML('<html><body><h1>Hello, world!</h1></body></html>');
+
+// Find the h1 node in the HTML template
+$h1_node = $dom->getElementsByTagName('h1')->item(0);
+
+// Output the modified HTML
+echo $dom->saveHTML();
+?>
